5cd1aab4ddb5de874b746846193a6b025a1f7cfa
Tushar <tusharbhatt0135@gmail.com>
2026-02-14 14:41:13 +0530

    chore: final details

--- page-builders/recipes-container/index.tsx
+++ page-builders/recipes-container/index.tsx
@@ -11,7 +11,7 @@ export default function RecipesContainer() {
     <div className="min-h-screen bg-neutral-900">
       <header className="border-b border-neutral-800 bg-neutral-900 px-4 py-4 sm:px-6">
         <div className="mx-auto max-w-3xl">
-          <div className="flex justify-between items-center">
+          <div className="flex justify-between sm:items-center items-end">
             <div>
               <h1 className="text-2xl font-bold tracking-tight text-white sm:text-3xl">
                 Discover recipes
@@ -20,12 +20,24 @@ export default function RecipesContainer() {
                 Search for cuisines, dishes, or ingredients
               </p>
             </div>
-            <a
-              href="https://tusharbhatt.vercel.app"
-              className="font-light underline text-neutral-400 hover:text-neutral-500"
-            >
-              Crafted by Tushar Bhatt
-            </a>
+            <div className="flex flex-col gap-1 sm:mt-0 mt-14 text-neutral-500">
+              <a
+                target="_blank"
+                aria-label="Crafted by"
+                href="https://tusharbhatt.vercel.app"
+                className="font-light hover:underline  hover:text-neutral-400 sm:text-md text-xs"
+              >
+                Crafted by Tushar Bhatt
+              </a>
+              <a
+                target="_blank"
+                aria-label="Repository link"
+                className="text-xs hover:underline text-neutral-500"
+                href="https://github.com/TusharBhatt1/cookbook"
+              >
+                See code here
+              </a>
+            </div>
           </div>
           <div className="mt-5">
             {/* https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout */}
